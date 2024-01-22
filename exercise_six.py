a73b18b1afbec5d81bb32613ca8cdada8f816027
Jhobert Buno <jhobertbuno02valdez@gmail.com>
2024-01-22 20:55:18 +0800

    pseudocode

---
+++ exercise_six.py
@@ -0,0 +1,7 @@
+# Exercise 6: Display numbers divisible by 5 from a list
+# Iterate the given list of numbers and print only those numbers which are divisible by 5
+
+# pseudocode
+# create functions
+# given list of nubers
+# print the result
