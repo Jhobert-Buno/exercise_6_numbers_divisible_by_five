67e40f5eb7188aedb9a05e8a32801084a590e5e4
Jhobert Buno <jhobertbuno02valdez@gmail.com>
2024-01-22 21:54:34 +0800

    asking user to input numbers

--- exercise_six.py
+++ exercise_six.py
@@ -8,6 +8,7 @@ def divfive (numbers):
         if divisible_by_five % 5 == 0:
             print(divisible_by_five)
 
+print("EXAMPLE")
 # given list of nubers
 given_list = [10, 20, 33, 46, 55]
 
@@ -16,3 +17,16 @@ print("The given numbers are: ", given_list)
 print("Numbers divisible by five: ")
 divfive(given_list)
 
+print(" ")
+# ask user to input numbers
+print("Please Input Five Numbers:", )
+num1=int(input("1st number: "))
+num2=int(input("2nd number: "))
+num3=int(input("3rd number: "))
+num4=int(input("4th number: "))
+num5=int(input("5th number: "))
+
+input_numbers = [num1,num2,num3,num4,num5]
+print("The numbers are: ", input_numbers)
+print("Numbers divisible by five: ")
+divfive(input_numbers)
