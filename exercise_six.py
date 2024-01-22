e4740e08783758fe900fe022afff0cd11778e240
Jhobert Buno <jhobertbuno02valdez@gmail.com>
2024-01-22 21:38:20 +0800

    create functions

--- exercise_six.py
+++ exercise_six.py
@@ -3,5 +3,10 @@
 
 # pseudocode
 # create functions
+def divfive (numbers):
+    for divisible_by_five in numbers:
+        if divisible_by_five % 5 == 0:
+            print(divisible_by_five)
+
 # given list of nubers
 # print the result
